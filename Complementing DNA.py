8a91a6cc84c5e2f7e75e03f3e09f1de7a629aadd
18lkent <18lkent@mlmc.vic.edu.au>
2017-11-23 11:43:33 +1100

    This program will display the complementary sequence to a sequence of dna

--- Complementing DNA.py
+++ Complementing DNA.py
@@ -1,6 +1,6 @@
 dnaSeq = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT" #The sequence being modified
-dnaSeq1 = dnaSeq.replace("A", "t").replace("G","c")
-dnaComp1 = dnaSeq1.replace("T","a").replace("C","g")
-newSeq = dnaComp1.upper()
-print("Original Sequence:\n"+dnaSeq)
-print("Complemntary DNA Sequence:\n"+newSeq)
+dnaSeq1 = dnaSeq.replace("A", "t").replace("G","c") #Replaces A's with lowercase t's and Uppercase G's with lowercase c's
+dnaComp1 = dnaSeq1.replace("T","a").replace("C","g") #Replaces Uppercase T's with lower case A's and Replaces Uppercase C's with lowercase g's
+newSeq = dnaComp1.upper() #Converts new sequence to uppercase
+print("Original Sequence:\n"+dnaSeq) #prints original sequence
+print("Complementary DNA Sequence:\n"+newSeq) #prints New Complementary Sequence
