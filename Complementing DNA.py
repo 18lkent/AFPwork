2e7fb3ef084bfb5675f4e2c2e0f4ed69ebf7d622
18lkent <18lkent@mlmc.vic.edu.au>
2017-11-23 11:37:24 +1100

    This program will display the complementary sequence to a sequence of dna

--- Complementing DNA.py
+++ Complementing DNA.py
@@ -1,3 +1,6 @@
 dnaSeq = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT" #The sequence being modified
-dnaSeq1 = dnaSeq.replace("A", "T")
-print(dnaSeq1) #prints final result
+dnaSeq1 = dnaSeq.replace("A", "t").replace("G","c")
+dnaComp1 = dnaSeq1.replace("T","a").replace("C","g")
+newSeq = dnaComp1.upper()
+print("Original Sequence:\n"+dnaSeq)
+print("Complemntary DNA Sequence:\n"+newSeq)
