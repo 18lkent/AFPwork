6e812d507a90a332dc1b56348dbd439664a30690
18lkent <18lkent@mlmc.vic.edu.au>
2017-11-30 14:27:04 +1100

    this is a funtions library including functions to convert degrees f to degrees c, distance in miles to kilometers and inches to centimeters

--- myFunctions.py
+++ myFunctions.py
@@ -1,14 +1,44 @@
-#create a main function that runs when this file is executed
-def main():
-    print('hello')
-    print("This program will ask you for a temperature\n in Fahrenheit")
-    print("____________________________________________")
-
-#create a function to convert fahrenheit to celcius
+#this function converts degrees in fahrenheit to celsius
 def ftoc():
     f = int(input("Enter Degrees in fahrenheit: "))
     cl = (f-32)*5/9
     print(f,"degrees fahrenheit converts to",cl,"degrees celsius")
 
-main()
-ftoc()
+#this function converts distance in miles to kilometres
+def mtk():
+    m = int(input("Enter distance in Miles: "))
+    k = (m*1.6)
+    print(m,"is",k,"kilometres")
+
+#this function converts distance in inches in inches to centimeters
+def itc():
+    i = int(input("Enter distance in Inches: "))
+    c = (i*2.54)
+    print(i,"is",c,"centimeters")
+
+#######################################################################################
+
+def menu():
+    choice = '0'
+    while choice == '0':
+        print("1 | Fahrenheit to Celsius Converter")
+        print("2 | Mile to Kilometre Converter")
+        print("3 | Inch to Centimeter Converter")
+        choice = input("Please make a choice: ")
+        if choice == "4":
+            print("Quitting...")
+            break
+        elif choice == "1":
+            ftoc()
+            menu()
+        elif choice == "2":
+            mtk()
+            menu()
+        elif choice == '3':
+            itc()
+            menu()
+        else:
+            print("I dont understand your input")
+            menu()
+menu()
+
